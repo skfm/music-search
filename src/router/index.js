f639a5b7feaafa3d98af775e1adfcd13e2977637
seiya <skkasans@gmail.com>
2020-08-21 07:09:29 +0900

    レイアウトの調整

--- src/router/index.js
+++ src/router/index.js
@@ -11,16 +11,14 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/result/:keyword',
+    name: 'result',
+    component: () => import('../views/Result.vue')
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
---
+++ src/views/Result.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          検索結果
+        </h1>
+
+        <p>
+          検索キーワード : {{ $route.params.keyword }}
+        </p>
+      </v-col>
+
+        <v-col
+          cols="12"
+        >
+          <v-card
+            color="black"
+            dark
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="title"
+                ></v-card-title>
+
+                <v-card-subtitle v-text="artistName"></v-card-subtitle>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile
+              >
+                <v-img></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title: 'アルバムタイトル',
+      artistName: 'アーティスト名',
+      image: '',
+    }
+  }
+}
+</script>
